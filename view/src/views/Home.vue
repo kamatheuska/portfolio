8d689a137fb8ff6a8cc6280ffbba411099c209f0
Nicolas Ramirez <nikameush@protonmail.com>
2019-09-08 21:19:56 +0200

    New VueJS app for the frontend

---
+++ view/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <div class="container">
+            <h1>Love u Marzi</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+    name: 'home'
+};
+</script>
