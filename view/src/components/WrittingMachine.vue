6d00efcdaa79a97e38d4b36fd523adb2f956dcf4
Nicolas Ramirez <nikameush@protonmail.com>
2019-09-13 17:54:45 +0200

    Testing deploy

--- view/src/components/WrittingMachine.vue
+++ view/src/components/WrittingMachine.vue
@@ -115,6 +115,8 @@ export default {
             .then(() => this.delay(1000))
             .then(() => this.resetText())
             .then(() => this.writeMachineText(this.banners.welcome, 100, 80))
+            .then(() => this.delay(2000))
+            .then(() => this.writeMachineText('And I\nlove\nMarzialina!', 100, 80))
             .catch((error) => {
                 console.error('Error in Machine', error)
             })
