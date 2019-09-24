8e0bbd8e150dd785995af9aed1aa68be6762be2a
Nicolas Ramirez <nikameush@protonmail.com>
2019-09-24 11:41:34 +0200

    feat(view): Added css file for reset styles

--- view/src/App.vue
+++ view/src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-
+import '@/assets/styles.css'
 export default {
     name: 'App',
     data() {
@@ -16,22 +16,3 @@ export default {
 };
 </script>
 
-<style lang="less">
-body, html {
-    height: 100vh;
-    width: 100vw;
-    margin: 0;
-    padding: 0;
-}
-html > * {
-    box-sizing: border-box;
-}
-#app {
-    height: 100%;
-    font-family: "Merriweather", serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
-</style>
