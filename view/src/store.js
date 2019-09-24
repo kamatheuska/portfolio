1bedf50a044741fb8403f0da12897469a01d8fef
Nicolas Ramirez <nikameush@protonmail.com>
2019-09-24 15:41:37 +0200

    fix(view): Setting animation to be done by default

--- view/src/store.js
+++ view/src/store.js
@@ -7,7 +7,7 @@ export default new Vuex.Store({
     state: {
         intro: {
             animation: {
-                done: false
+                done: true
             },
             skipIntro: false
         }
