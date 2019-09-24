f3c449fa52e736bfd6e8f2f53019a9b9e85653ca
Nicolas Ramirez <nikameush@protonmail.com>
2019-09-24 15:41:57 +0200

    Adding lazy loading to views

--- view/src/router.js
+++ view/src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
-import Intro from './views/Intro.vue';
 
 Vue.use(Router);
 
@@ -12,19 +11,21 @@ export default new Router({
         {
             path: '/',
             name: 'intro',
-            component: Intro
+            component: () => import(/* webpackChunkName: "intro" */ './views/Intro.vue')
         },
         {
             path: '/home',
             name: 'home',
-            component: Home
+            component: () => import(/* webpackChunkName: "home" */ './views/Home.vue')
+        },
+        {
+            path: '/projects',
+            name: 'projects',
+            component: () => import(/* webpackChunkName: "projects" */ './views/Projects.vue')
         },
         {
             path: '/about',
             name: 'about',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
             component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
         }
     ]
