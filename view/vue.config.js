72f4f2b1047dab87b7b4c501328d8b7aec3f216c
Nicolas Ramirez <nikameush@protonmail.com>
2019-09-29 16:43:31 +0200

    feat(view): Added alias to webpakc build

--- view/vue.config.js
+++ view/vue.config.js
@@ -1,3 +1,12 @@
+const path = require('path');
+
 module.exports = {
-    outputDir: '../server/public'
+    outputDir: '../server/public',
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': path.join(__dirname, 'src')
+            }
+        }
+    }
 }
