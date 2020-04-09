25d95eb7a583ab2717d0ce7b5b77f5005ac9cbfd
Nicolas Ramirez <nicolas.ramirez.ka@icloud.com>
2020-04-09 19:26:18 +0200

    Added react public folder

---
+++ server/publicReact/precache-manifest.cc8de319b0e81ea33076a05ed9cff7ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64266ca01f3455488d84f2a0a049914",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20b7687a854652eaf35e",
+    "url": "/static/css/main.9565dcf6.chunk.css"
+  },
+  {
+    "revision": "be239aac9e45e8fb3c8b",
+    "url": "/static/js/2.23ced6d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.23ced6d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20b7687a854652eaf35e",
+    "url": "/static/js/main.8d417c93.chunk.js"
+  },
+  {
+    "revision": "963c730af879aa1dca9f",
+    "url": "/static/js/runtime-main.4d55df52.js"
+  }
+]);
