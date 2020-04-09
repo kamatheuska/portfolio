c011feb1dc9cf0bffc1680d76938967fc0a5e1e2
Nicolas Ramirez <nicolas.ramirez.ka@icloud.com>
2020-04-09 19:27:04 +0200

    Removed  env variables

--- server/config/index.js
+++ server/config/index.js
@@ -11,21 +11,4 @@ const getEnvVar = function (key, defaultValue = null) {
 module.exports = {
     port: getEnvVar('PORT', 5000),
     nodeEnv: getEnvVar('NODE_ENV', 'development'),
-    darkskyUrl: getEnvVar('DARKSKY_URL'),
-    geocodeApiKey: getEnvVar('GEOCODE_API_KEY'),
-    geocodeUrl: getEnvVar('GEOCODE_URL'),
-    twitch: {
-        id: getEnvVar('TWITCH_CLIENT_ID'),
-        baseUrl: getEnvVar('TWITCH_API_URL', 'https://api.twitch.tv/helix'),
-        users: [
-            'ESL_SC2',
-            'OgamingSC2',
-            'cretetion',
-            'freecodecamp',
-            'storbeck',
-            'habathcx',
-            'RobotCaleb',
-            'noobs2ninjas'
-        ]
-    }
 }
