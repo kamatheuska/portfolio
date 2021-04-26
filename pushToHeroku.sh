3ca143f0d280dcc6ed5e2c92b22c4cbfb2220e01
Nicolas Ramirez <nico9017@gmail.com>
2021-04-27 00:04:51 +0200

    removing everything

--- pushToHeroku.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd view && npm run build && cd ../ && git add --all && git commit -m 'New build'
-git subtree push --prefix server heroku master
--- view/src/utils.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export function sleep(delay) {
-    return new Promise((resolve) => setTimeout(resolve, delay));
-}
