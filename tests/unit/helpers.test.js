90121a7186135f63619a43611a4ae72393da57d2
Nicolas Ramirez <nico9017@gmail.com>
2021-05-01 19:08:02 +0200

    skipped and added some tests

--- tests/unit/helpers.test.js
+++ tests/unit/helpers.test.js
@@ -3,22 +3,22 @@ const {
     truthyArguments
 } = require('../../helpers')
 
-test('HELPERS FUNCTIONS', { skip: false }, (t) => {
-    t.test('test truthyArguments, which must return a boolean', function (assert) {
+test(':: 🏁  HELPERS', { skip: false }, (t) => {
+    t.test(':::: 🚩 truthyArguments', function (assert) {
         let bar = 'Some string'
         let foobar = { params: { units: 'si', b: undefined, exclude: 'minutely,hourly,daily,alerts,flags' } }
 
         assert.equal(truthyArguments([false, true, bar]), false,
-            'Function must return a value of false if one of the arguments is falsy')
+            '🤖 Function must return a value of false if one of the arguments is falsy')
 
         assert.equal(truthyArguments([foobar, true, 'some string']), true,
-            'Function must return a value of false if one of the arguments is an object')
+            '🤖 Function must return a value of false if one of the arguments is an object')
 
         assert.equal(truthyArguments([true, bar]), true,
-            'Function must return a value of true if all of the arguments are true')
+            '🤖 Function must return a value of true if all of the arguments are true')
 
         assert.equal(truthyArguments([2, true, undefined]), false,
-            'Function must return a value of false if one of the arguments is undefined')
+            '🤖 Function must return a value of false if one of the arguments is undefined')
 
         assert.end()
     })
