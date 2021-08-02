22e52ef1c25cd5d6e85fcf048b29d693d07eac74
Nicolas Ramirez <nico9017@gmail.com>
2021-08-02 23:30:47 +0200

    test 3

--- view/buildView.sh
+++ view/buildView.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+
+
 cd view &&
+  ls -la . &&
+  echo "*********************************************" &&
+  echo "*** LISTING VIEW node_modules before BUILD***" &&
+  echo "*********************************************" &&
   export NPM_CONFIG_PRODUCTION=true &&
   export NODE_ENV=production &&
   npm run build;
