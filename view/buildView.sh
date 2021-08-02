e2d26ac3915f4987149eebe0df05857ca7666268
Nicolas Ramirez <nico9017@gmail.com>
2021-08-02 23:32:18 +0200

    pipeline

--- view/buildView.sh
+++ view/buildView.sh
@@ -3,10 +3,13 @@
 
 
 cd view &&
+  echo "********************************" &&
+  echo "*** LISTING VIEW before BUILD***" &&
+  echo "********************************" &&
   ls -la . &&
-  echo "*********************************************" &&
-  echo "*** LISTING VIEW node_modules before BUILD***" &&
-  echo "*********************************************" &&
+  echo "********************************" &&
+  echo "*** LISTING VIEW node_modules ***" &&
+  echo "********************************" &&
   export NPM_CONFIG_PRODUCTION=true &&
   export NODE_ENV=production &&
   npm run build;
