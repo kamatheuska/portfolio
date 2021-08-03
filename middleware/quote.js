1895310fdfcdacac95b9b5436d76292fffd42b9a
Nicolas Ramirez <nico9017@gmail.com>
2021-08-03 19:44:53 +0200

    adding quotes file

---
+++ middleware/quote.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const router = express.Router();
+const movieQuotes = require('movie-quotes');
+const { throwNewError } = require('./errors');
+
+const { isString } = require('../helpers');
+
+router.get('/api/quote', (req, res, next) => {
+    const fullQuote = movieQuotes.random();
+    const endOfQuote = fullQuote.indexOf('"', 1) + 1;
+    const parsedQuote = {
+        text: fullQuote.slice(0, endOfQuote),
+        author: fullQuote.slice(endOfQuote + 1),
+    };
+    if (isString(parsedQuote.text) && isString(parsedQuote.author)) {
+        res.status(200).json(parsedQuote);
+    } else {
+        next(throwNewError('Error on Server. No quotes found', 'ValidationError'));
+    }
+});
+
+module.exports = router;
