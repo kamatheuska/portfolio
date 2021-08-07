ccc4d344af760604d0e0134a5a8717fba820d6d1
Nicolas Ramirez <nico9017@gmail.com>
2021-08-07 17:01:34 +0200

    refactore url stuff

--- controllers/urlShortener.js
+++ controllers/urlShortener.js
@@ -1,16 +1,11 @@
 const router = require('express').Router();
 
-const { getUrl } = require('../services/urlShortener');
-const { createUrl } = require('../middleware/urlShortener');
+const { createUrl, getUrl } = require('../middleware/urlShortener');
 
-router.get('/:id', async (req, res, next) => {
-    const { id } = req.params;
-    try {
-        const url = await getUrl(id);
-        res.redirect(302, url.original);
-    } catch (error) {
-        next(error);
-    }
+router.get('/:id', getUrl, (req, res) => {
+    const { url } = res.locals;
+
+    res.redirect(302, url.original);
 });
 
 router.post('/new', createUrl, (req, res) => {
--- middleware/urlShortener.js
+++ middleware/urlShortener.js
@@ -3,16 +3,23 @@ const {
     checkHostnameValidity,
     saveUrl,
     createUrlObject,
+    getUrlById,
 } = require('../services/urlShortener');
 const { getHostNameFromUrl } = require('../utils/url');
 
 async function createUrl(req, res, next) {
     const { url } = req.body;
+    const { logInfo } = res.locals;
     const hostname = getHostNameFromUrl(url);
     try {
         await checkHostnameValidity(hostname);
+        logInfo(`Hostname ${hostname} validated succesfully`);
+
         const model = await buildNewShortUrl(url);
+        logInfo('Saved Url in db', { hostname, newUrl: model });
+
         const doc = await saveUrl(model);
+        logInfo('Saved doc:', doc);
 
         res.locals.urlData = createUrlObject(doc, req);
 
@@ -22,4 +29,18 @@ async function createUrl(req, res, next) {
     }
 }
 
+async function getUrl(req, res, next) {
+    const { id } = req.params;
+    const { logInfo } = res.locals;
+    try {
+        res.locals.url = await getUrlById(id);
+        logInfo('Url found', res.locals.url);
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
 exports.createUrl = createUrl;
+exports.getUrl = getUrl;
