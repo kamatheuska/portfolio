c9bbd3f1dbd123cb02c0fab191ea3facfc809425
Nicolas Ramirez <nico9017@gmail.com>
2021-08-08 00:13:27 +0200

    introduced config serialization and new variables

--- config/index.js
+++ config/index.js
@@ -5,13 +5,16 @@ const isTest = process.env.NODE_ENV === 'test';
 const isDevelopment = process.env.NODE_ENV === 'development';
 let $config;
 
-function getEnvVar(key, defaultValue = null) {
+function getEnvVar(key, defaultValue = null, type) {
     if (process.env[key]) {
+        if (type === Boolean) return process.env[key] === 'true'; // Coerce to boolean
+        if (type === Number) return +process.env[key]; // Coerce to number
         return process.env[key];
     }
     if (defaultValue) {
         return defaultValue;
     }
+
     throw new Exception('init.getEnvVar', `Required but not defined : Env Variable ${key}.`);
 }
 
@@ -28,8 +31,14 @@ function initConfig() {
     }
 
     $config = {
-        port: getEnvVar('PORT', 5000),
+        port: getEnvVar('PORT', 5000, Number),
         nodeEnv: getEnvVar('NODE_ENV', 'development'),
+        debugMode: getEnvVar('DEBUG_MODE', 'false', Boolean),
+        db: {
+            url: {
+                documentLimit: getEnvVar('DB_URL_DOCUMENT_LIMIT', 100, Number),
+            },
+        },
         mongoDbUri,
     };
 }
