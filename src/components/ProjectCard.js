7188c529a90b1144f858c976c28d0453bfd072c0
Nicolas Ramirez <nico9017@gmail.com>
2021-09-25 15:46:43 +0200

    fixing home project cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -13,12 +13,12 @@ export default function ProjectCard({
         <p className="card-header-title">{title}</p>
       </header>
       <div className="card-content">
-        <div className="card-image">
+        <div className="card-image mb-5">
           <figure className="image">
             <img src={thumbnailImage} alt="Placeholder" />
           </figure>
         </div>
-        <div className="content">{description}</div>
+        <div className="content mt-5">{description}</div>
       </div>
       <footer className="card-footer">
         <Link to={path} className="card-footer-item">
