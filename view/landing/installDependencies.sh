f44cc0b66aeae048ffe3de4c23cef6ab55a8ef93
Nicolas Ramirez <nico9017@gmail.com>
2021-11-09 12:52:37 +0100

    optimizing up heroku build with package json

--- view/landing/installDependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd view &&
-  export NPM_CONFIG_PRODUCTION=false &&
-  export NODE_ENV= &&
-  echo NPM_CONFIG_PRODUCTION &&
-  echo ${NPM_CONFIG_PRODUCTION} &&
-  NPM_CONFIG_PRODUCTION=false NODE_ENV=development npm install --production=false;
