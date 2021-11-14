2320040ccb85f191b0136c56feafafd7d7ed9a3d
Nicolas Ramirez <nico9017@gmail.com>
2021-11-14 18:14:25 +0100

    landing:fixes on view and install

--- view/landing/src/views/mini-services/UrlShortener.vue
+++ view/landing/src/views/mini-services/UrlShortener.vue
@@ -3,8 +3,8 @@
         <header>
             <h1 class="title is-3">Give me a shorter one</h1>
         </header>
-        <section class="is-flex is-justify-content-center">
-            <div class="box" v-if="!resultUrl">
+        <section>
+            <div class="url-shortener__form-box box" v-if="!resultUrl">
                 <p class="block">
                     {{ explanation }}
                 </p>
@@ -21,16 +21,18 @@
             </div>
             <div v-else class="has-text-centered">
                 <p class="block">Now you can access</p>
-                <p class="block">
+                <p class="url-shortener__original block p-3">
                     <strong>{{ shortendOriginalUrl }}</strong>
                 </p>
                 <p class="block">going to</p>
-                <p class="block">
-                    <a :href="response.href" target="_blank">
-                        <strong> {{ resultUrl }} </strong>
-                    </a>
-                </p>
-                <div style="width: 14rem; margin: 0 auto">
+                <a :href="response.href" target="_blank">
+                    <div class="url-shortener__short box has-background-primary has-text-white m-4">
+                        <p class="block">
+                            <strong> {{ resultUrl }} </strong>
+                        </p>
+                    </div>
+                </a>
+                <div class="mt-5" style="width: 14rem; margin: 0 auto">
                     <figure class="image is-square">
                         <img :src="resultGifSrcImage" />
                     </figure>
@@ -73,10 +75,12 @@ export default {
     },
 
     methods: {
+        createShortUrl,
+
         async onSubmit() {
             try {
                 const { origin } = window.location;
-                this.response = await createShortUrl(this.form.url);
+                this.response = await this.createShortUrl(this.form.url);
                 this.resultUrl = `${origin}${this.response.href}`;
             } catch (error) {
                 console.error(error);
@@ -87,6 +91,9 @@ export default {
 </script>
 <style lang="scss" scoped>
 .url-shortener {
+    @include tablet {
+        padding-left: 0;
+    }
     header {
         padding-left: 1rem;
         @include tablet {
@@ -96,10 +103,28 @@ export default {
     section {
         padding-top: 10rem;
         padding-bottom: 10rem;
+        @include tablet {
+            display: flex;
+            justify-content: center;
+        }
+    }
 
-        .box {
-            flex-basis: 40rem;
-            padding: 4rem 6rem;
+    &__form-box.box {
+        flex-basis: 40rem;
+        padding: 4rem 6rem;
+    }
+
+    &__original {
+        max-width: 100%;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+    }
+
+    &__short {
+        transition: transform 300ms ease;
+        &:hover {
+            transform: scale(1.2);
         }
     }
 }
