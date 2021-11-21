76a731cde8a78f36730637b8d5872e4a29a3f2d6
Nicolas Ramirez <nico9017@gmail.com>
2021-11-21 20:03:26 +0100

    api: moving functions outside module.exports

--- middleware/index.js
+++ middleware/index.js
@@ -2,52 +2,56 @@ const { throwNewError } = require('./errors');
 const { isString, isObject } = require('../helpers');
 const { getApiPayload, sendApiRequest } = require('../utils');
 
-module.exports = {
-    forceSsl(req, res, next) {
-        if (req.header('x-forwarded-proto') !== 'https') {
-            res.redirect(`https://${req.header('host')}${req.url}`);
-        } else {
+function forceSsl(req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(301, ['https://', req.get('Host'), req.url].join(''));
+    }
+    next();
+}
+
+function validateWeatherPayload(req, res, next) {
+    if (!isObject(req.body)) {
+        throwNewError('Payload is not an Object', 'ValidationError');
+    }
+    const payload = req.body;
+    const payloadKeys = Object.keys(payload);
+    payloadKeys.forEach((key) => {
+        if (!isString(payload[key])) {
+            throwNewError('Error on Payload', 'ValidationError');
+        }
+    });
+
+    next();
+}
+
+function buildWeatherPayload(req, res, next) {
+    const { url, config } = getApiPayload()(req.body);
+    Object.defineProperty(req, 'axiosPayload', { value: { url, config } });
+    next();
+}
+
+function getGeocodeData(req, res, next) {
+    if (!isString(req.body.address)) {
+        throwNewError('Address in request is not a string', 'ValidationError');
+    }
+    const { address } = req.body;
+    sendApiRequest('geocode')(address)
+        .then((response) => {
+            const coords = response.body.features[0].geometry.coordinates;
+            Object.defineProperty(req, 'axiosPayload', {
+                value: getApiPayload()({
+                    lng: coords[0],
+                    lat: coords[1],
+                }),
+            });
             next();
-        }
-    },
+        })
+        .catch(next);
+}
 
-    validateWeatherPayload(req, res, next) {
-        if (!isObject(req.body)) {
-            throwNewError('Payload is not an Object', 'ValidationError');
-        }
-        const payload = req.body;
-        const payloadKeys = Object.keys(payload);
-        payloadKeys.forEach((key) => {
-            if (!isString(payload[key])) {
-                throwNewError('Error on Payload', 'ValidationError');
-            }
-        });
-
-        next();
-    },
-
-    buildWeatherPayload(req, res, next) {
-        const { url, config } = getApiPayload()(req.body);
-        Object.defineProperty(req, 'axiosPayload', { value: { url, config } });
-        next();
-    },
-
-    getGeocodeData(req, res, next) {
-        if (!isString(req.body.address)) {
-            throwNewError('Address in request is not a string', 'ValidationError');
-        }
-        const { address } = req.body;
-        sendApiRequest('geocode')(address)
-            .then((response) => {
-                const coords = response.body.features[0].geometry.coordinates;
-                Object.defineProperty(req, 'axiosPayload', {
-                    value: getApiPayload()({
-                        lng: coords[0],
-                        lat: coords[1],
-                    }),
-                });
-                next();
-            })
-            .catch(next);
-    },
+module.exports = {
+    buildWeatherPayload,
+    forceSsl,
+    getGeocodeData,
+    validateWeatherPayload,
 };
