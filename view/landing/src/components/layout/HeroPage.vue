2bfbb4eeb73f6f6821911057e559856f63f9be05
Nicolas Ramirez <nico9017@gmail.com>
2021-11-24 00:10:56 +0100

    landing: components added for layout

---
+++ view/landing/src/components/layout/HeroPage.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <section class="hero is-medium">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <p class="title is-1">{{ title }}</p>
+                </div>
+            </div>
+        </section>
+        <section class="section" v-for="(section, i) in sections" :key="`hero-page-section-${i}`">
+            <div class="columns">
+                <div class="column">
+                    <p
+                        class="block is-size-5 has-text-justified"
+                        v-for="(paragraph, j) in section"
+                        :key="`hero-page-paragraph-${i}-${j}`"
+                    >
+                        {{ paragraph }}
+                    </p>
+                </div>
+            </div>
+        </section>
+        <slot name="after-sections"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        /**
+         * @description
+         * should be an array of arrays
+         * First array level is sections
+         * Second array level is paragraphs
+         */
+        sections: {
+            type: Array,
+            default: () => [],
+        },
+    },
+};
+</script>
---
+++ view/landing/src/components/layout/LevelSection.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="section is-medium">
+        <h2 v-if="title" class="title is-3 has-text-centered mb-6">{{ title }}</h2>
+        <HorizontalLevel>
+            <LevelItem v-for="(item, i) in items" :key="`level-item-${i}`" v-bind="item" />
+        </HorizontalLevel>
+    </section>
+</template>
+
+<script>
+import HorizontalLevel from '@/components/layout/HorizontalLevel.vue';
+import LevelItem from '@/components/LevelItem.vue';
+
+export default {
+    components: {
+        HorizontalLevel,
+        LevelItem,
+    },
+
+    props: {
+        items: {
+            type: Array,
+            default: () => [],
+        },
+        title: String,
+    },
+};
+</script>
