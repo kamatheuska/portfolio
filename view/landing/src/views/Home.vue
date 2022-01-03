f5e0618a7acea3258253282e74a372dac1312cf2
Nicolas Ramirez <nico9017@gmail.com>
2022-01-03 22:56:59 +0100

    landing: added cookie to check if visitor was already on the page

--- view/landing/src/views/Home.vue
+++ view/landing/src/views/Home.vue
@@ -14,36 +14,7 @@
                 </div>
             </div>
         </section>
-        <Portal>
-            <transition name="fade-in">
-                <div class="home__polygon-ray--container" v-if="showUpperPolygon">
-                    <svg
-                        class="responsive-svg home__polygon-ray home__polygon-ray--link"
-                        version="1.1"
-                        viewBox="0 0 1872.2 649.84"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path d="m25.329 1178 1844.9 242.71-248.57-2191.3-1620 532.17z" />
-                    </svg>
-                    <svg
-                        class="responsive-svg home__polygon-ray home__polygon-ray--warning"
-                        version="1.1"
-                        viewBox="0 0 1872.2 649.84"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path d="m25.329 1178 1844.9 242.71-248.57-2191.3-1620 532.17z" />
-                    </svg>
-                    <svg
-                        class="responsive-svg home__polygon-ray"
-                        version="1.1"
-                        viewBox="0 0 1872.2 649.84"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path d="m25.329 1178 1844.9 242.71-248.57-2191.3-1620 532.17z" />
-                    </svg>
-                </div>
-            </transition>
-        </Portal>
+        <PolygonRayAnimation :show="showUpperPolygon" />
         <Portal>
             <transition name="fade-in">
                 <div v-if="showSubtitle" class="home__bg-polygon"></div>
@@ -55,15 +26,23 @@
 <script>
 import { mapMutations } from 'vuex';
 import { Portal } from '@linusborg/vue-simple-portal';
-import HomeAnimation from '@/components/HomeAnimation.vue';
-// import BackgroundPolygon from '@/components/svg/BackgroundPolygon.vue';
+import * as cookies from '@/constants/cookies';
+
+import HomeAnimation from '@/components/animations/HomeAnimation.vue';
+import PolygonRayAnimation from '@/components/animations/PolygonRayAnimation.vue';
 
 export default {
     name: 'Home',
     components: {
-        // BackgroundPolygon,
         HomeAnimation,
         Portal,
+        PolygonRayAnimation,
+    },
+
+    computed: {
+        isNewVisitor() {
+            return this.$route.query.new === 'false' || this.$getCookie(cookies.SAW_INTRO);
+        },
     },
 
     data: () => ({
@@ -83,43 +62,20 @@ export default {
         },
 
         checkAnimationQuery() {
-            if (this.$route.query.new === 'false') return;
+            if (this.isNewVisitor) return;
             this.animateText = true;
         },
     },
 
     mounted() {
         this.checkAnimationQuery();
+
+        this.$setCookie(cookies.SAW_INTRO, true);
     },
 };
 </script>
 
 <style lang="scss" scoped>
-$skew-duration: 15s;
-$shrink-duration: 10s;
-
-@keyframes shrink {
-    0% {
-        transform: scale(1);
-    }
-    100% {
-        transform: scale(0.5);
-    }
-}
-@keyframes skew {
-    0% {
-        transform: skew(0deg);
-    }
-    10% {
-        transform: skew(15deg);
-    }
-    50% {
-        transform: scale(-0.8);
-    }
-    80% {
-        transform: skew(0, 15deg);
-    }
-}
 .home {
     /*
     * Bulma Overrides
@@ -159,54 +115,6 @@ $shrink-duration: 10s;
 
         @include tablet {
             --path__first-point: 20%;
-        }
-    }
-    &__polygon-ray {
-        width: 100%;
-        height: 100%;
-        fill: lighten($p-pink, 4%);
-        stroke-width: 10px;
-        animation: skew infinite;
-        animation-duration: calc($skew-duration);
-        animation-timing-function: ease-out;
-        position: absolute;
-        opacity: 0.7;
-
-        @include tablet {
-            stroke-width: 5px;
-        }
-        &--link {
-            fill: lighten($link, 10%);
-            animation: skew infinite;
-            animation-duration: calc($skew-duration * 2);
-            position: absolute;
-        }
-
-        &--warning {
-            fill: lighten($warning, 35%);
-            animation: skew infinite;
-            animation-duration: calc($skew-duration * 3);
-            position: absolute;
-        }
-
-        &--container {
-            top: -5rem;
-            right: 15rem;
-
-            position: absolute;
-            width: 15rem;
-            height: 18rem;
-            z-index: 1000;
-            animation: shrink $shrink-duration;
-            animation-iteration-count: infinite;
-            animation-direction: alternate;
-
-            @include tablet {
-                top: -2rem;
-                left: 4rem;
-                width: 25rem;
-                height: 36rem;
-            }
         }
     }
 }
