2d1f99a66c17d52e7479c3b9f052321613f4bf11
Nicolas Ramirez <nikamuesh@protonmail.com>
2022-04-04 17:05:27 +0200

    removing unused stuff

--- postinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-set -o errexit    # always exit on error
-set -o pipefail   # don't ignore exit codes when piping output
-
-lerna bootstrap --scope server --ignore-prepublish
-lerna bootstrap --ignore-prepublish --ignore=server -- --production=false
