83fa40433b3736c3b50ff6de616fdf2369c6e9c6
Nicolas Ramirez <nicolas@matizlab.com>
2023-02-19 17:54:25 +0100

    feat(landing): add component DefaultLayout ref: 59

---
+++ apps/landing/src/lib/components/layouts/DefaultLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <slot name="menu">
+            <main-menu />
+        </slot>
+        <div>
+            <slot name="content"></slot>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import MainMenu from '../menus/MainMenu.vue';
+</script>
