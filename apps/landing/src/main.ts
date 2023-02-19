551002999ed74def3f2fd9a21f360f7bbcb9f4d5
Nicolas Ramirez <nicolas@matizlab.com>
2023-02-19 18:46:52 +0100

    feat(landing): add reset css ref: 59

--- apps/landing/src/main.ts
+++ apps/landing/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
-import './style.css';
+
+import './lib/assets/styles/reset.css';
+import './lib/assets/styles/main.css';
+
 import App from './App.vue';
 import { appRouter } from './lib/router';
 
