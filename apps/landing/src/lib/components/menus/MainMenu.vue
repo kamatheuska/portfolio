3221a43dd968e90a4a32ef598a2313e6c9297e30
Nicolas Ramirez <nicolas@matizlab.com>
2023-03-22 16:01:16 +0100

    use stars menu on main menu

--- apps/landing/src/lib/components/menus/MainMenu.vue
+++ apps/landing/src/lib/components/menus/MainMenu.vue
@@ -1,17 +1,40 @@
 <template>
     <div :class="$style.root">
-        <LamaratiLogo />
-        <NavBar :items="navigationConfig" />
+        <div :class="$style.modal">
+            <div :class="$style.container">
+                <div :class="$style.logo">
+                    <LamaratiLogo />
+                </div>
+            </div>
+        </div>
+        <stars-menu />
     </div>
 </template>
 
 <script lang="ts" setup>
-import { navigationConfig } from '../../config/menus/navigation.menu';
 import LamaratiLogo from '../logos/LamaratiLogo.vue';
-import NavBar from '../navbar/NavBar.vue';
+import StarsMenu from './StarsMenu.vue';
 </script>
 
 <style module>
 .root {
+    position: relative;
+}
+.modal {
+    height: 100vh;
+    width: 100vw;
+    position: absolute;
+    background-color: white;
+}
+
+.container {
+    display: flex;
+    height: 100%;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+}
+.logo {
+    font-size: 2.3rem;
 }
 </style>
