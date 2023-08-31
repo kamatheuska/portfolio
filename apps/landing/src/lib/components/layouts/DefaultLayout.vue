dc0277e4e1a6ffcb81d2b4f61262d69a7d3821df
Nicolas Ramirez <nicolas@matizlab.com>
2023-08-31 14:28:04 +0200

    center content

--- apps/landing/src/lib/components/layouts/DefaultLayout.vue
+++ apps/landing/src/lib/components/layouts/DefaultLayout.vue
@@ -5,8 +5,10 @@
                 <main-menu :hide-at-start="hideMainMenuAtStart" />
             </div>
         </slot>
-        <div :class="$style.content">
-            <slot name="content"></slot>
+        <div :class="$style.container">
+            <div :class="$style.content">
+                <slot name="content"></slot>
+            </div>
         </div>
     </div>
 </template>
@@ -26,5 +28,14 @@ defineProps({
     padding-top: var(--default-layout-padding-top);
     padding-left: var(--default-layout-padding-left);
     padding-right: var(--default-layout-padding-right);
+}
+@media (min-width: 768px) {
+    .content {
+        max-width: 600px;
+    }
+    .container {
+        display: flex;
+        justify-content: center;
+    }
 }
 </style>
