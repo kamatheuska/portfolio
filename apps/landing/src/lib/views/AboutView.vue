f3669a2d071b1edf0e59c7d288ecb2962d8a775b
Nicolas Ramirez <nicolas@matizlab.com>
2023-09-07 13:17:36 +0200

    add link to udk

--- apps/landing/src/lib/views/AboutView.vue
+++ apps/landing/src/lib/views/AboutView.vue
@@ -5,10 +5,19 @@
                 <main>
                     <p>
                         For the past years, I have been working as a developer on an
-                        <a href="https://www.lidl.de" target="_blank"> e-commerce company</a>, a
-                        <a href="https://www.cloudblue.com/" target="_blank">startup project</a> on a large corporation,
-                        a <a href="https://www.affluent.io/">marketing focused startup</a> and a
-                        <a href="https://en.c-981.com/">zero code SaaS startup</a>.
+                        <a href="https://www.lidl.de" target="_blank" rel="noopener noreferrer"> e-commerce company</a>,
+                        a
+                        <a href="https://www.cloudblue.com/" target="_blank" rel="noopener noreferrer"
+                            >startup project</a
+                        >
+                        on a large corporation, a
+                        <a href="https://www.affluent.io/" target="_blank" rel="noopener noreferrer"
+                            >marketing focused startup</a
+                        >
+                        and a
+                        <a href="https://en.c-981.com/" target="_blank" rel="noopener noreferrer"
+                            >zero code SaaS startup</a
+                        >.
                     </p>
                     <p>
                         Although my main focus is in frontend development and its technologies, I was required to do a
@@ -18,8 +27,9 @@
                     <p>The last few years I worked in Fullstack Development roles.</p>
                     <p>
                         I have an artistic background, studying music composition in Berlin in the Musikschule
-                        Friedrichshain-Kreuzberg and the UdK Berlin. After a while, artistic endeavour wasn't enough, so
-                        I decided to learn to code.
+                        Friedrichshain-Kreuzberg and the
+                        <a href="https://www.udk-berlin.de/en" target="_blank" rel="noopener noreferrer">UdK Berlin</a>.
+                        After a while, artistic endeavour wasn't enough, so I decided to learn to code.
                     </p>
                     <p>
                         After all this time, I can see how the rigurosity of german music theory influence my love for
