3e69eebc7fe5d1ef4ec15a359f2af03f7bf319cb
Nicolas Ramirez <nicolas@matizlab.com>
2023-10-20 11:17:21 +0200

    style adjustments for back button

--- apps/landing/src/lib/components/BackButton.vue
+++ apps/landing/src/lib/components/BackButton.vue
@@ -3,7 +3,7 @@
         <span>
             <font-awesome-icon :icon="['fa', 'arrow-left']" />
         </span>
-        back
+        BACK
     </button>
 </template>
 
@@ -21,13 +21,26 @@
     position: fixed;
     justify-content: center;
     top: 0;
-    font-size: 2rem;
+    font-size: 1rem;
     left: 20px;
     font-family: var(--font-family-mono);
+    font-weight: 900;
+    letter-spacing: 3px;
+    cursor: pointer;
 }
 
 .root span {
-    font-size: 2rem;
-    margin-right: 2rem;
+    font-size: 1rem;
+    margin-right: 1rem;
+}
+
+@media (min-width: 768px) {
+    .root {
+        font-size: 1rem;
+    }
+    .root span {
+        font-size: 1rem;
+        margin-right: 1rem;
+    }
 }
 </style>
