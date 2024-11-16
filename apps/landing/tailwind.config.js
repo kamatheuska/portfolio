2856b08e17aae31c070409a780aa08fcf2ed48ab
Nicolas Ramirez <nicolas@matizlab.com>
2024-11-16 18:20:00 +0100

    landing: index working

--- apps/landing/tailwind.config.js
+++ apps/landing/tailwind.config.js
@@ -2,7 +2,38 @@
 export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "light-blue": "#95BCCC",
+                pink: {
+                    100: "#fef8f8",
+                    200: "#fef1f1",
+                    300: "#fdeaea",
+                    400: "#fde3e3",
+                    500: "#fcdcdc",
+                    600: "#cab0b0",
+                    700: "#978484",
+                    800: "#655858",
+                    900: "#322c2c",
+                },
+                "dark-blue": "#2B4C59",
+                "darker-blue": "hsl(197, 35%, 18%)",
+                brown: "#988080",
+                gray: "#BECEDA",
+                green: "hsl(178, 100%, 55%)",
+                "dark-green": "hsl(178, 20%, 35%)",
+                "light-brown": "#DDD4D4",
+                "light-green": "#b5fffd",
+            },
+            fontFamily: {
+                mono: ["Fira Mono", "monospace"],
+                sans: ["Roboto", "sans-serif"],
+                serif: ["Playfair Display", "serif"],
+            },
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(circle, var(--tw-gradient-stops))",
+            },
+        },
     },
     plugins: [],
 };
